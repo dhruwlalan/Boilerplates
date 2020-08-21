615a7361e3de6cd712eb6e4160d09301ab848db3
dhruw <dhruwlalan22@gmail.com>
2020-08-21 23:01:47 +0530

    Webpack Basic Configured

--- WebTemplates/WebpackBasic/src/js/index.js
+++ WebTemplates/WebpackBasic/src/js/index.js
@@ -1,19 +1,1 @@
 import './assets.js';
-import $ from "jquery";
-import moment from 'moment';
-
-const greet = (name) => {
-	return `Welcome, ${name}!`;
-}
-
-document.getElementById('greet-btn').addEventListener('click', () => {
-	const input = $('#input').val();
-	const now = moment().toString();
-	console.log(now);
-	if (input) {
-		const output = greet(input);
-		document.getElementById('output').textContent = output;
-	} else {
-		document.getElementById('output').textContent = '';
-	}	
-});
--- WebTemplates/WebpackBasic/webpack.start.js
+++ WebTemplates/WebpackBasic/webpack.start.js
@@ -1,36 +1,29 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-	mode: 'production' ,
+	mode: 'development' ,
 	devtool: 'source-map' ,
 	entry: { index: './src/js/index.js' } ,
 	output: {
-		filename: '[name].[contentHash].bundle.js' ,
+		filename: '[name].bundle.js' ,	
 		path: path.resolve(__dirname, 'dist')
 	} ,
 	module: {
 		rules: [
 			{ test: /\.js$/, exclude: /node_modules/, use: ['babel-loader'] } ,
-			{ test: /\.scss$/ , use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'] } ,
+			{ test: /\.scss$/ , use: ['style-loader', 'css-loader', 'sass-loader'] } ,
 			{ test: /\.html$/ , use: ['html-loader'] } , 
 			{ test: /\.(jpeg|png|jpg|gif)$/ , use: {loader: 'file-loader' , options: {name: '[name].[ext]' , outputPath: 'assets/images'}} },
 			{ test: /\.svg$/ , use: {loader: 'file-loader' , options: {name: '[name].[ext]' , outputPath: 'assets/svg'}} },
 			{ test: /\.ico$/ , use: {loader: 'file-loader' , options: {name: 'favicon.ico' , outputPath: 'assets/favicon'}} },
 		]
-	} ,
+	},
 	plugins: [
-		new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'src', 'index.html') }) ,
-		new MiniCssExtractPlugin({ filename: 'style.[contentHash].css' }) ,
-		new CleanWebpackPlugin()
-	] ,
+		new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'src', 'index.html') }),
+	],
 	optimization: {
-		minimizer: [ new OptimizeCssAssetsPlugin() , new TerserPlugin() ],
-		splitChunks: {
+        splitChunks: {
             cacheGroups: {
                 vendors: {
                     test: /[\\/]node_modules[\\/]/,
@@ -40,5 +33,5 @@ module.exports = {
                 }
             }
         }
-	}
+    },
 };
