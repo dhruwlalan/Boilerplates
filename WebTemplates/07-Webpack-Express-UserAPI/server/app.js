dfa447d74d22c905e3afec6449a60158b2701801
dhruw <dhruwlalan22@gmail.com>
2020-12-02 19:32:52 +0530

    Add: Webpack Express with UserAPI Template

---
+++ WebTemplates/07-Webpack-Express-UserAPI/server/app.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const express = require('express');
+const basic = require('./middlewares/basic');
+const setSecurityHeaders = require('./middlewares/setSecurityHeaders');
+const connectWebpack = require('./middlewares/connectWebpack');
+const errorController = require('./controllers/errorController');
+const userRouter = require('./routes/userRoutes');
+
+
+/*Create Express App*/
+const app = express();
+
+
+/* ** Setup Security Middlewares ** */
+setSecurityHeaders(app);
+/* ** Hook webpack-dev-middleware with hot reload ***/
+connectWebpack(app);
+/* ** Use Basic Middlewares ** */
+basic.setupTemplateEngin(app);
+basic.serverStaticFiles(app);
+basic.useParsers(app);
+basic.gzipResponses(app);
+
+
+/*Define Routes*/
+app.use( '/api/users' , userRouter );
+app.get( '/' , (req , res) => {
+	res.status(200).render('index' , {
+		msg: 'Hello, World!!' ,
+	});
+});
+
+
+/*Handle Undefined Routes*/
+app.all( '*' , (req , res , next) => {
+	res.status(404).json({
+		status: '404' ,
+		unknownRoute: req.originalUrl ,
+	});
+});
+
+
+/*Global Error Handler Middleware, executed when passed argument inside next()*/
+app.use(errorController);
+
+module.exports = app;
