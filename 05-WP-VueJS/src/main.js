b0a1bed44c214f86b8e871d134e24cf31b9ace61
dhruw <dhruwlalan22@gmail.com>
2021-01-04 20:08:25 +0530

    rf: wp-vuejs

--- 05-WP-VueJS/src/main.js
+++ 05-WP-VueJS/src/main.js
@@ -1,6 +1,11 @@
+///ASSETS///
 import './assets/assets';
+
+///MAIN///
 import { createApp } from 'vue';
 
 import App from './App.vue';
 
-createApp(App).mount('#app');
+const app = createApp(App);
+
+app.mount('#app');
